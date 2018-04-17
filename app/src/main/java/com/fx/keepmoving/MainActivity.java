6d12a096707269fbda198a2fbf07bbf481f55508
shashanihao <121314516@qq.com>
2018-04-17 20:24:15 +0800

    2018年4月17日19:59:18

--- app/src/main/java/com/fx/keepmoving/MainActivity.java
+++ app/src/main/java/com/fx/keepmoving/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         //git测试2018年4月17日14:04:26s  客户端提交测试
         //Android 提交测试  Android本地提交测试
         //Android提交测试
-        //家里提交测试
         setContentView(R.layout.activity_main);
     }
 }
