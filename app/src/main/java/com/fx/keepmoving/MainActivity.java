2fd64195105eec7ba3f4a2add728e5e000ced914
616385187 <616385187@qq.com>
2018-04-17 15:36:52 +0800

    **
    客户端提交测试

--- app/src/main/java/com/fx/keepmoving/MainActivity.java
+++ app/src/main/java/com/fx/keepmoving/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //git测试2018年4月17日14:04:26s  客户端提交测试
         setContentView(R.layout.activity_main);
     }
 }
