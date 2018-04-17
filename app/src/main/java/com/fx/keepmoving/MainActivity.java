f4280dbc23abb36e239db1d6f3175b1fcfaeec34
616385187 <616385187@qq.com>
2018-04-17 15:31:46 +0800

    Update MainActivity.java

--- app/src/main/java/com/fx/keepmoving/MainActivity.java
+++ app/src/main/java/com/fx/keepmoving/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//2018年4月17日15:31:33
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
