644c8c9f5506e3ba5fbdc7e06cc30dbef070d7f0
616385187 <616385187@qq.com>
2018-04-18 09:32:07 +0800

    账号测试
    2018年4月18日09:32:04

--- app/src/main/java/com/fx/keepmoving/MainActivity.java
+++ app/src/main/java/com/fx/keepmoving/MainActivity.java
@@ -1,4 +1,4 @@
-package com.fx.keepmoving;
+﻿package com.fx.keepmoving;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //git测试2018年4月17日14:04:26s  客户端提交测试
         //Android 提交测试  Android本地提交测试
-        //Android提交测试
+        //Android提交测试**************
         setContentView(R.layout.activity_main);
     }
 }
